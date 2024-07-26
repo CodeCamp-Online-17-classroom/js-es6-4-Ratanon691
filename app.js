9913be877b4d977939f976e3bfc7b126361173fa
Non T. <117605943+Ratanon691@users.noreply.github.com>
2024-07-26 10:23:31 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-// write code here
+const nums1 = [1, -2, 3, 4];
+const nums2 = [8, 3, -8, 1];
+
+const combinedArray = [5, ...nums1, -6, -1, ...nums2];
+
+console.log(combinedArray); // [5, 1, -2, 3, 4, -6, -1, 8, 3, -8, 1]
+
+const sum = combinedArray.reduce((acc, num) => acc + num, 0);
+
+console.log(sum); // The sum of all elements
